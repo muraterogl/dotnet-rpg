3d553d2b5c9a41b07c05984efdd7c185418180aa
Murat <murat51392@gmail.com>
2021-05-01 07:43:48 +0300

    Auth in character service, and weapon model

---
+++ Models/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Models
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public Character Character { get; set; }
+        public int CharacterId { get; set; } //EF knows weapons is the child of one to one relationship by this prop
+    }
+}
